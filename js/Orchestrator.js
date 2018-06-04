74e86f2143955d98b0de3b83341ce64320bca55c
Ben Brooks <ben@benbrooks.net>
2018-06-03 20:57:55 -0700

    randomized exercises

--- js/Orchestrator.js
+++ js/Orchestrator.js
@@ -3,34 +3,16 @@ import { sounds } from "./sounds";
 
 export class Orchestrator {
     constructor() {
-        this.sounds = sounds;
-        return this;
+        this.restLength = 5 * 1000;
+        this.workoutLength = 30 * 1000;
     }
     
     async go() {
-        // WIP
-        await new PromiseAudio(this.sounds.transitions.next).play()
-        await new PromiseAudio(this.sounds.exercises['plank']).play()
-        await this.wait(2000);
-        await new PromiseAudio(this.sounds.exercises['plank']).play()        
-        await new PromiseAudio(this.sounds.transitions.go).play()
-        await this.wait(5000);
-        await new PromiseAudio(this.sounds.transitions.rest).play()
-        await new PromiseAudio(this.sounds.transitions.next).play()
-        await new PromiseAudio(this.sounds.exercises['crunches']).play()
-        await this.wait(2000);
-        await new PromiseAudio(this.sounds.exercises['crunches']).play()        
-        await new PromiseAudio(this.sounds.transitions.go).play()
-        await this.wait(5000);
-        await new PromiseAudio(this.sounds.transitions.rest).play()
-        await new PromiseAudio(this.sounds.transitions.next).play()
-        await new PromiseAudio(this.sounds.exercises['squats']).play()
-        await this.wait(2000);
-        await new PromiseAudio(this.sounds.exercises['squats']).play()        
-        await new PromiseAudio(this.sounds.transitions.go).play()
-        await this.wait(5000);
-        await new PromiseAudio(this.sounds.transitions.rest).play()
-        await new PromiseAudio(this.sounds.transitions.complete).play()
+        const exercises = this.getExercises()
+        for (let i = 0; i < exercises.length; i++) {
+            await this.playExercise(exercises[i])
+        }
+        this.play(sounds.transitions.complete);
     }
 
     wait(milliseconds) {
@@ -38,4 +20,33 @@ export class Orchestrator {
           setTimeout(() => resolve(milliseconds), milliseconds);
         });
     };
+
+    shuffle(array) {
+        const a = array.slice();
+        for (let i = a.length - 1; i > 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1));
+            [a[i], a[j]] = [a[j], a[i]];
+        }
+        return a;
+    }
+
+    getExercises() {
+        return this.shuffle(Object.values(sounds.exercises));
+    }
+
+    async playExercise(path) {
+        await this.play(sounds.transitions.next);
+        await this.play(path);
+        await this.wait(this.restLength);
+        await this.play(path);        
+        await this.play(sounds.transitions.go);
+        await this.wait(this.workoutLength);
+        return this.play(sounds.transitions.rest);;
+    }
+
+    play(path) {
+        return new PromiseAudio(path).play();
+    }
+
+
 }
