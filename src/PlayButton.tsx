94fdc77d4d26c1c12db44563bbea7d4e02dbbc3f
Ben Brooks <ben@benbrooks.net>
2018-06-09 20:34:32 -0700

    place react-icons (800k) with emojis

--- src/PlayButton.tsx
+++ src/PlayButton.tsx
@@ -1,16 +1,19 @@
 import * as React from 'react';
-import { FaPauseCircle, FaPlayCircle } from 'react-icons/lib/fa';
+import { emojiEnabled } from './utils';
 
 interface IProps {
     playing: boolean
     onClick: any;
 }
 
+const playText = emojiEnabled() ? "▶" : "Play"
+const pauseText = emojiEnabled() ? "⏸" : "Pause"
+
 class PlayButton extends React.Component<IProps> {
     public render() {
         return (
             <button style={{ fontSize: 120 }} onClick={this.props.onClick}>
-                {this.props.playing ? <FaPauseCircle /> : <FaPlayCircle />}
+                {this.props.playing ? pauseText : playText}
             </button>
         );
     }
--- src/utils.ts
+++ src/utils.ts
@@ -1,8 +1,29 @@
-export function shuffle (array: any[]): any[] {
+export function shuffle(array: any[]): any[] {
     const a = array.slice();
     for (let i = a.length - 1; i > 0; i--) {
         const j = Math.floor(Math.random() * (i + 1));
         [a[i], a[j]] = [a[j], a[i]];
     }
     return a;
+}
+
+/**
+ * Determine if this browser supports emoji.
+ * https://gist.github.com/mathisonian/7885295
+ */
+export function emojiEnabled() {
+    let context;
+    let smile;
+    if (!document.createElement('canvas').getContext) {
+        return;  
+    } 
+    context = document.createElement('canvas').getContext('2d');
+    if (typeof context!.fillText !== 'function') {
+        return;
+    }
+    smile = String.fromCharCode(55357) + String.fromCharCode(56835);
+    context!.textBaseline = "top";
+    context!.font = "32px Arial";
+    context!.fillText(smile, 0, 0);
+    return context!.getImageData(16, 16, 1, 1).data[0] !== 0;
 }
